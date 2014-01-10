7dd70a34686ab98d61e15a3879d3bb6b18750fb0
Benjamin Kovach <bkovach5@uga.edu>
2014-01-10 11:39:12 -0800

    update bottom border

--- js/Utils.js
+++ js/Utils.js
@@ -1,8 +1,8 @@
 function autoResizeDiv(){
     var m_height = $("#main").height(); 
     if (m_height < window.innerHeight){
-        $("#main").css("height", window.innerHeight + 'px');
-        $("#main_body").css("height", (window.innerHeight - 270) + 'px');
+        $("#main").css("height", window.innerHeight - 5 + 'px');
+        $("#main_body").css("height", (window.innerHeight - 280) + 'px');
     }
 }
 
--- site.hs
+++ site.hs
@@ -11,6 +11,7 @@ import qualified Text.Blaze.Html5            as H
 import           Text.Blaze.Html5.Attributes
 import qualified Text.Blaze.Html5.Attributes as A
 --------------------------------------------------------------------------------
+
 main :: IO ()
 main = hakyll $ do
     {- Build Tags (unused for now, but handy) -}
